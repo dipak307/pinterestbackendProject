d1aed299e6f45bdf22e3e946afe70cacc7d73763
dipak307 <deepzende876@gmail.com>
2024-02-13 16:18:31 +0530

    first backend

---
+++ routes/users.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+const plm=require('passport-local-mongoose');
+
+mongoose.connect("mongodb://127.0.0.1:27017/pinterestdata");
+
+const userSchema = new mongoose.Schema({
+  username:String,
+  name:String,
+  email:String,
+  password:  String,
+  contact:Number,
+  profileImage:String,
+  boards:{
+    type:Array,
+    default:[]
+  },
+  posts:[{
+    type:mongoose.Schema.Types.ObjectId,
+    ref:"post"
+  }]
+});
+
+userSchema.plugin(plm);
+
+module.exports = mongoose.model('user', userSchema);
+
+
+
